19ea3b86f364ad6cdb0108b9ff1750cfe806d64c
Jonah Lazar <lazarprezsmerjonas@gmail.com>
2022-09-17 19:36:47 +0300

    project setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Dashboard
+
+Making a dashboard portofolio project with using first time Tailwindcss.
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ".index.html",
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
